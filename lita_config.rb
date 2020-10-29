dd7ed1f6c3cb193ad98c2c0aa8b3e4523e24606e
jerryDog23 <73463867+jerryDog23@users.noreply.github.com>
2020-10-29 20:54:52 +0800

    Update lita_config.rb

--- lita_config.rb
+++ lita_config.rb
@@ -19,17 +19,20 @@ Lita.configure do |config|
   # appropriate gem to the Gemfile.
   # heroku uses a RACK_ENV of 'production' by default
   if ENV['RACK_ENV'] == 'production'
-    config.robot.adapter = :slack
+  	config.robot.adapter = :slack
   	config.redis[:url] = ENV.fetch('REDIS_URL')
   else
-	  config.robot.adapter = :shell
+	config.robot.adapter = :shell
   end
   # slack adapter demands a value even in dev when we aren't using it... 
   config.adapters.slack.token = ENV.fetch('SLACK_TOKEN', '')
   ## Example: Set options for the chosen adapter.
   # config.adapter.username = "myname"
   # config.adapter.password = "secret"
-
+  Lita.configure do |config|
+	config.robot.adapter = :slack
+	config.adapters.slack.token = "xoxb-1468758027297-1468522978004-yRHd67fv7zulLBiG4Hrddv7C"
+  end
   ## Example: Set options for the Redis connection.
   # config.redis.host = "127.0.0.1"
   # config.redis.port = 1234
